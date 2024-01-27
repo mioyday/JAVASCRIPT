0c11a6ea10a2e3b3ddfb4e4e175f08805ac6ee76
mioyday <mioyday@gmail.com>
2024-01-27 21:33:54 +0900

    function

---
+++ JAVASCRIPT/1.basic.js
@@ -0,0 +1,18 @@
+//사용예제1
+function add(num1, num2){
+    return result = num1 + num2;
+}
+const result = add(1, 2);
+console.log(result);
+
+//사용예제2
+function fullName(firstName, lastName){
+    return '${firstName} ${lastName}';
+}
+let lastname ='김';
+let firstName = '지수';
+console.log(fullName(firstName, lastname));
+
+let lastname2 ='빅';
+let firstName2 = '철수';
+console.log(fullName(firstName2, lastname2));
